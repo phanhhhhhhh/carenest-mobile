bb2e0bf57df046932e142d4e087b320e9b33e5da
phanhhhhhhh <nguyenphananh49@gmail.com>
2026-06-23 00:47:44 +0700

    fix: skip Firebase init on web, make FirebaseAuth nullable for Chrome

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -5,6 +6,8 @@ import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (!kIsWeb) {
+    await Firebase.initializeApp();
+  }
   runApp(const ProviderScope(child: CareNestApp()));
 }
