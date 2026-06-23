352b526c3794801dbe18c8302b840cf527171b26
phanhhhhhhh <nguyenphananh49@gmail.com>
2026-06-23 14:48:53 +0700

    chore: update session state 2026-06-23 14:48

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   if (!kIsWeb) {
     await Firebase.initializeApp();
   }
